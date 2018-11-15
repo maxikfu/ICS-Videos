ef673561174bb72775f168b13f79fe87922edafc
maxikfu <maxikfu@gmail.com>
2018-11-15 16:20:07 -0600

    fixed bag in pdf2text, added result of this function to git

--- chapter_preprocessing.py
+++ chapter_preprocessing.py
@@ -9,10 +9,11 @@ def pdf2text(pdf_file_path):  # converts pdf file to txt file
     env = dict(os.environ)
     env['LC_ALL'] = 'en_US.UTF-8'
     out_fname = os.path.splitext(os.path.basename(pdf_file_path))[0]
-    cmd = ['pdftotext', pdf_file_path, os.path.join(os.path.dirname(os.path.abspath(pdf_file_path)), out_fname+'.txt')]
+    working_dir = os.path.dirname(os.path.abspath(pdf_file_path))
+    cmd = ['pdftotext', pdf_file_path, os.path.join(working_dir, out_fname+'.txt')]
     p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
     out, err = p.communicate()
-    with codecs.open(os.path.join(out_fname+'.txt'), 'r', encoding='utf-8') as f_in:
+    with codecs.open(os.path.join(working_dir, out_fname+'.txt'), 'r', encoding='utf-8') as f_in:
         content = f_in.read()
     content.replace('\r', '').replace('\x0C', '')
     return content
