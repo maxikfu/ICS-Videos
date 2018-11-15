ce4ccf56c8b6ea56102c6b5710ce507ff95d9052
megorov <megorov@central.uh.edu>
2018-11-15 16:16:41 -0600

    added path to the current directory then creatin pdf2text

--- chapter_preprocessing.py
+++ chapter_preprocessing.py
@@ -9,7 +9,7 @@ def pdf2text(pdf_file_path):  # converts pdf file to txt file
     env = dict(os.environ)
     env['LC_ALL'] = 'en_US.UTF-8'
     out_fname = os.path.splitext(os.path.basename(pdf_file_path))[0]
-    cmd = ['pdftotext', pdf_file_path, os.path.join(out_fname+'.txt')]
+    cmd = ['pdftotext', pdf_file_path, os.path.join(os.path.dirname(os.path.abspath(pdf_file_path)), out_fname+'.txt')]
     p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
     out, err = p.communicate()
     with codecs.open(os.path.join(out_fname+'.txt'), 'r', encoding='utf-8') as f_in:
