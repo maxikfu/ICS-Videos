0f5beda343b9e14a86d47c6033da1f1366046c9b
megorov <megorov@central.uh.edu>
2018-11-15 16:09:36 -0600

    pdf parsing and sentence selections

--- chapter_preprocessing.py
+++ chapter_preprocessing.py
@@ -1,30 +1,25 @@
-import PyPDF2
+import codecs
+import os, subprocess
+from config import Config
+
 from nltk import word_tokenize, sent_tokenize, pos_tag
 
-def pdf_reader(pdf_file_path):  # returns sentences from pdf
-    # creating a pdf file object
-    pdfFileObj = open(pdf_file_path, 'rb')
 
-    # creating a pdf reader object
-    pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+def pdf2text(pdf_file_path):  # converts pdf file to txt file
+    env = dict(os.environ)
+    env['LC_ALL'] = 'en_US.UTF-8'
+    out_fname = os.path.splitext(os.path.basename(pdf_file_path))[0]
+    cmd = ['pdftotext', pdf_file_path, os.path.join(out_fname+'.txt')]
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+    out, err = p.communicate()
+    with codecs.open(os.path.join(out_fname+'.txt'), 'r', encoding='utf-8') as f_in:
+        content = f_in.read()
+    content.replace('\r', '').replace('\x0C', '')
+    return content
 
-    # number of pages in pdf file
-    pages = pdfReader.numPages
+if __name__ == '__main__':
+    pdf2text('data/IE_chapter17.pdf')
 
-    text =""
-    # creating a page object
-    # for i in pages:
-    #     pageObj = pdfReader.getPage(i)
-    pageObj = pdfReader.getPage(0)
-    # extracting text from page
-    text = pageObj.extractText()
-    print(word_tokenize(text))
-    # closing the pdf file object
-    pdfFileObj.close()
+# work_sentence = sent_tokenize(text)[168]
 
-
-with open('data/IE_chapter17.txt', 'r') as f:
-    text = f.read()
-work_sentence = sent_tokenize(text)[168]
-
-print(pos_tag(word_tokenize(work_sentence)))
+# print(pos_tag(word_tokenize(work_sentence)))
