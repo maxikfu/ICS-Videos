34a2eaceae791c94d4e7cf395f65d4b7f3a2c0bd
maxikfu <maxikfu@gmail.com>
2018-11-18 23:36:12 -0600

    still working on adjusting book text

--- GFQG.py
+++ GFQG.py
@@ -1,7 +1,8 @@
 import utility
-
+import collections as coll
 import re
 import spacy
+import numpy as np
 from spacy.lang.en import English
 from spacy.tokens import Doc
 import pprint
@@ -9,39 +10,91 @@ nlp = spacy.load('en')
 
 
 def data_cleaning(raw_text):  # cleaning text
-    # sentences = sent_tokenize(text)
-    # looking for the titles of subchapters and related to that subchapter content
+    # Returns - lis of Span objects sentences from Doc object
+    # and chapter name
+    print(raw_text)
+    exit()
+    raw_text = re.sub(r"[\n]", " ", raw_text)
     doc = nlp(raw_text)
-    tokenizer = English().Defaults.create_tokenizer(nlp)
-    sentences = [sent.string.strip() for sent in doc.sents]
-    # deleting sentences ending with ,
-    # sentences = [re.sub(r"[\n]", " ", s) for s in sentences if s[-1] != ',']
-    sent_containing_chapter = [s[s.find('CHAPTER')+len('CHAPTER'):] for s in sentences if 'CHAPTER' in s]
-    chapter_name = re.sub(r"[0-9]", "", sent_containing_chapter[0]).strip()
-    chapter_number = re.sub(r"[a-zA-Z]", "", sent_containing_chapter[0]).strip()
-    # sub_topics = [s for s in sentences if '17.' in s] # todo: figure out method to extract subtopics
-    tokenized_sentences = []
-    for doc in tokenizer.pipe(sentences, batch_size=50):
-        tokenized_sentences.append(doc)
-    # tokenized_sentences = [s for s in tokenized_sentences if len(s) > 3]
+    c = 1
+    for s in doc.sents:
+        print(c, str(s).strip())
+        c += 1
+    exit()
+    raw_text = raw_text.split()
+    raw_text = ' '.join(raw_text)
+    text = re.sub(r"[\n]", " ", raw_text)
+    text = re.sub(r"[0-9]", "", text)
+    raw_text = re.sub(r"[\W\n]", " ", raw_text)
+    raw_text = re.sub(r"[\n]", "", raw_text)
+    doc = nlp(text.lower())
+    freq_word = {}
+    for t in doc:
+        if t.is_stop or t.is_punct:
+            pass
+        else:
+            if str(t).strip() in freq_word:
+                freq_word[str(t).strip()] += 1
+            else:
+                freq_word[str(t).strip()] = 1
+    s = [(k, freq_word[k]) for k in sorted(freq_word, key=freq_word.get, reverse=True)]
+    for k, v in s:
+        print(k,v)
+    exit()
+    chapter = False
+    for sentence in doc.sents:
+        if chapter:
+            chapter_name = sentence
+            break
+        elif 'CHAPTER' in str(sentence):  # next sentence suppose to be name of the chapter
+            chapter = True
+            chapter_number = re.sub(r"[a-zA-Z]", "", str(sentence))
+    doc = nlp(raw_text)
+    tokenized_sentences = [s for s in doc.sents]
     return tokenized_sentences, chapter_name
 
 
 def sentence_selection(data, chapter):  # returns selected sentences with biggest score
     # Here we will calculate all features needed for sentence selection
+    # Parameters - data - list of sentences (Span objects of Doc type)
     # f1 - number of tokens similar in the title/ length of sentence (excluding punctuation marks)
     # f2 - does sentence containes any abbreviation
     # f3 - contain a word in its superlative degree
-    chapter = chapter.lower()
+    # f4 - beginning with discorse connective TODO: figure out how to identify them
+    # f5 - number of words in sentence
+    # f6= number of nouns / length of the sentence
+    # f7 - number of pronouns/ length of the sentence
+    chapter = str(chapter).lower()
     chapter = chapter.split()
+    counter = 0
+    weights = [1, 0.1, 1, 1, 0.0001, 1, 1]
+    score = []
     for s in data:
-
-        # si = [re.sub(r"[^0-9a-zA-Z\s]", "", str(i)) for i in s if not i.is_punct]
-        # f1 = len([i for i in si if str(i).lower() in chapter])/len(si)
-        # f2 = len([i for i in si if str(i).isupper() and len(i) > 1])
-        # f3 = len([token for token in s if token.tag_ == 'JJS'])
         print(s)
-        print([token.pos_ for token in s])
+    #     features = []
+    #     f3 = 0
+    #     pos_tags = [token.tag_ for token in s]
+    #     number_of_words = len([pos for pos in pos_tags if pos != 'PUNCT'])
+    #     f1 = 1/(len([i for i in s if str(i).lower() in chapter])/len(s)+1)
+    #     features.append(f1)
+    #     f2 = len([i for i in s if i.text.isupper() and len(i.text) > 1 and i.pos_ == 'PROPN'])
+    #     features.append(f2)
+    #     if 'JJS' in pos_tags:
+    #         f3 = 1
+    #     features.append(f3)
+    #     if any(discorse in str(s) for discorse in ['the following', 'The following', 'example', 'So', 'above', 'Figure', 'like this one']):
+    #         f4 = 0
+    #     else:
+    #         f4 = 1
+    #     features.append(f4)
+    #     f5 = number_of_words
+    #     features.append(f5)
+    #     f6 = len([p for p in pos_tags if p in ['NN', 'NNS']])/number_of_words
+    #     features.append(f6)
+    #     f7 = len([p for p in pos_tags if p in ['NNP', 'NNPS']])/number_of_words
+    #     features.append(f7)
+    #     score.append(np.dot(weights, features))
+    # z = [print(y, x) for y, x in sorted(zip(score, data))]
     return data
 
 
@@ -51,4 +104,4 @@ if __name__ == '__main__':
     with open('data/IE_chapter17.txt', 'r') as f:
         book_text = f.read()
     sentences, c_name = data_cleaning(book_text)
-    # sentence_selection(sentences, c_name)
+    sentence_selection(sentences, c_name)
