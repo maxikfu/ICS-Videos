2cb8bbf38f306b2e17155ae90933229676dbf40d
megorov <megorov@central.uh.edu>
2018-11-19 16:41:24 -0600

    Step of sentence selection implemented

--- GFQG.py
+++ GFQG.py
@@ -9,99 +9,90 @@ import pprint
 nlp = spacy.load('en')
 
 
-def data_cleaning(raw_text):  # cleaning text
-    # Returns - lis of Span objects sentences from Doc object
-    # and chapter name
-    print(raw_text)
-    exit()
-    raw_text = re.sub(r"[\n]", " ", raw_text)
+def data_pre_processing(raw_text):  # cleaning text
+    # returns Dictionary of spans with topic as a key
+    # identifying topic and related content of the topic
     doc = nlp(raw_text)
-    c = 1
-    for s in doc.sents:
-        print(c, str(s).strip())
-        c += 1
-    exit()
-    raw_text = raw_text.split()
-    raw_text = ' '.join(raw_text)
-    text = re.sub(r"[\n]", " ", raw_text)
-    text = re.sub(r"[0-9]", "", text)
-    raw_text = re.sub(r"[\W\n]", " ", raw_text)
-    raw_text = re.sub(r"[\n]", "", raw_text)
-    doc = nlp(text.lower())
-    freq_word = {}
-    for t in doc:
-        if t.is_stop or t.is_punct:
-            pass
-        else:
-            if str(t).strip() in freq_word:
-                freq_word[str(t).strip()] += 1
-            else:
-                freq_word[str(t).strip()] = 1
-    s = [(k, freq_word[k]) for k in sorted(freq_word, key=freq_word.get, reverse=True)]
-    for k, v in s:
-        print(k,v)
-    exit()
-    chapter = False
+    document_content_dict = {}  # dict: key - topic name, value - list of Span objects
     for sentence in doc.sents:
-        if chapter:
-            chapter_name = sentence
-            break
-        elif 'CHAPTER' in str(sentence):  # next sentence suppose to be name of the chapter
-            chapter = True
-            chapter_number = re.sub(r"[a-zA-Z]", "", str(sentence))
-    doc = nlp(raw_text)
-    tokenized_sentences = [s for s in doc.sents]
-    return tokenized_sentences, chapter_name
+        if len(sentence) > 1:
+            if any(x in str(sentence) for x in ['Topic:', 'Subtopic:']):  # topic found
+                topic = str(sentence).replace('Topic:', '')
+                topic = topic.replace('Subtopic:', '').strip()
+                topic = topic.replace('.', '').strip().lower()
+                document_content_dict[topic] = []
+            else:
+                document_content_dict[topic].append(sentence)
+    return document_content_dict
 
 
-def sentence_selection(data, chapter):  # returns selected sentences with biggest score
-    # Here we will calculate all features needed for sentence selection
-    # Parameters - data - list of sentences (Span objects of Doc type)
-    # f1 - number of tokens similar in the title/ length of sentence (excluding punctuation marks)
-    # f2 - does sentence containes any abbreviation
+def sentence_selection(data_dict):  # returns selected sentences depending on score
+    # Here we will calculate all features needed for sentence selection.
+    # For each chapter we will select sentences what are good for gap fill questions and most informative
+    # Parameters - data_dict - spans of sentences with topic as key  (Span objects of Doc type)
+    # f1 - number of tokens similar in the title/ length of sentence (excluding punctuation marks) I look for words in all titles
+    # f2 - does sentence contains any abbreviation
     # f3 - contain a word in its superlative degree
-    # f4 - beginning with discorse connective TODO: figure out how to identify them
-    # f5 - number of words in sentence
+    # f4 - beginning with discourse connective TODO: figure out how to identify them
+    # f5 - number of words in sentence (excluding stop words)
     # f6= number of nouns / length of the sentence
     # f7 - number of pronouns/ length of the sentence
-    chapter = str(chapter).lower()
-    chapter = chapter.split()
-    counter = 0
-    weights = [1, 0.1, 1, 1, 0.0001, 1, 1]
-    score = []
-    for s in data:
-        print(s)
-    #     features = []
-    #     f3 = 0
-    #     pos_tags = [token.tag_ for token in s]
-    #     number_of_words = len([pos for pos in pos_tags if pos != 'PUNCT'])
-    #     f1 = 1/(len([i for i in s if str(i).lower() in chapter])/len(s)+1)
-    #     features.append(f1)
-    #     f2 = len([i for i in s if i.text.isupper() and len(i.text) > 1 and i.pos_ == 'PROPN'])
-    #     features.append(f2)
-    #     if 'JJS' in pos_tags:
-    #         f3 = 1
-    #     features.append(f3)
-    #     if any(discorse in str(s) for discorse in ['the following', 'The following', 'example', 'So', 'above', 'Figure', 'like this one']):
-    #         f4 = 0
-    #     else:
-    #         f4 = 1
-    #     features.append(f4)
-    #     f5 = number_of_words
-    #     features.append(f5)
-    #     f6 = len([p for p in pos_tags if p in ['NN', 'NNS']])/number_of_words
-    #     features.append(f6)
-    #     f7 = len([p for p in pos_tags if p in ['NNP', 'NNPS']])/number_of_words
-    #     features.append(f7)
-    #     score.append(np.dot(weights, features))
-    # z = [print(y, x) for y, x in sorted(zip(score, data))]
-    return data
+
+    # lemmatizing and removing stop words from names of all topics
+    all_the_topics = ' '.join([t for t in data_dict])
+    topics_doc = nlp(all_the_topics)
+    all_the_topics = set(token.lemma_ for token in topics_doc if not token.is_stop and not token.is_punct)
+    weights = [1.5, 0.1, 0.2, 0.5, 0.001, 0.2, 0.1]
+    result_sel_sent = {}
+    for topic, sentences in data_dict.items():
+        result_sel_sent[topic] = []
+        # topic = 'named entity recognition'
+        # sentences = data_dict['named entity recognition']
+        for span in sentences:
+            number_of_words = len(set(token.lemma_ for token in span if not token.is_stop and not token.is_punct))
+            score = 0
+            if number_of_words > 4:
+                features = []
+                pos_tags = [token.tag_ for token in span]
+                f1 = len([i for i in span if str(i.lemma_).lower() in all_the_topics])/number_of_words
+                features.append(f1)
+                f2 = 1/np.exp(len([i for i in span if i.text.isupper() and len(i.text) > 1 and i.pos_ == 'PROPN']))
+                features.append(f2)
+                f3 = 0
+                if 'JJS' in pos_tags:
+                    f3 = 1
+                features.append(f3)
+                if any(discourse in str(span) for discourse in ['the following', 'The following', 'example', 'So', 'above', 'Figure', 'like this one', 'Fig.', 'These', 'This', 'That']):
+                    f4 = 0
+                else:
+                    f4 = 1
+                features.append(f4)
+                f5 = number_of_words
+                features.append(f5)
+                f6 = len([p for p in pos_tags if p in ['NN', 'NNS']])/number_of_words
+                features.append(f6)
+                f7 = len([p for p in pos_tags if p in ['NNP', 'NNPS']])/number_of_words
+                features.append(f7)
+                score = np.dot(weights, features)
+                # print(score, features, span)
+            result_sel_sent[topic].append(score)
+        # z = [print(y, x) for y, x in sorted(zip(score, data))]
+
+    # in this step we do selection based on the score. At this moment boundary set to 1.1
+    result_sentences = {}
+    for topic in data_dict:
+        selection = set(score for score in result_sel_sent[topic] if score > 1.2)
+        if selection:
+            result_sentences[topic] = [data_dict[topic][result_sel_sent[topic].index(elem)] for elem in selection]
+    return result_sentences
 
 
 if __name__ == '__main__':
     # utility.pdf2text('data/IE_chapter17.pdf')
     # after converting pdf to txt we need to clean up data
-    with open('data/IE_chapter17.txt', 'r') as f:
+    with open('data/IE_chapter17_cleaned.txt', 'r') as f:
         book_text = f.read()
-    sentences, c_name = data_cleaning(book_text)
-    sentence_selection(sentences, c_name)
+    data = data_pre_processing(book_text)
+    score = sentence_selection(data)
+    # for i in range(0, len(data['named entity recognition'])):
+    #     print(score['named entity recognition'][i], data['named entity recognition'][i])
