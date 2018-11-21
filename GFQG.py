861799d3cf307f46faf2f54592ef6c726e798989
megorov <megorov@central.uh.edu>
2018-11-21 16:06:24 -0600

    generation gap-filled questions with multiple answers DONE

--- GFQG.py
+++ GFQG.py
@@ -1,13 +1,8 @@
-import utility
-import collections as coll
-import re
 import spacy
 import numpy as np
-from spacy.lang.en import English
-from spacy.tokens import Doc
-import pprint
-nlp = spacy.load('en')
+from random import shuffle
 
+nlp = spacy.load('en')  # make sure to use larger model!
 
 # overwriting spaCy, making it not case sensitive
 def is_stop(word):
@@ -94,7 +89,7 @@ def sentence_selection(data_dict):  # returns selected sentences depending on sc
     # in this step we do selection based on the score. At this moment boundary set to 1.1
     result_sentences = {}
     for topic in data_dict:
-        selection = set(score for score in result_sel_sent[topic] if score > 1.3)
+        selection = set(score for score in result_sel_sent[topic] if score > 1.5)
         if selection:
             result_sentences[topic] = [data_dict[topic][result_sel_sent[topic].index(elem)] for elem in selection]
     return result_sentences, all_the_topics
@@ -111,6 +106,45 @@ def token_dep_height(tokens):  # height of the token in syntactic tree
     return depth
 
 
+def distractor_selection(key, key_sentence, document, word_count):
+    """
+    Distractor is a concept semantically close to the key which, howerver, cannot serve as the right answer itself
+
+    :features:
+    f1 - context - measure of contextual similarity
+    f2 - dice coefficient score between question sentence and sentence containing distractor
+    f3 - difference in term frequencies of distractor and key
+
+    :param key: noun chunk answer for the gap question
+    :param document: document itself used to find distractors
+    :param word_count: count of words
+    :return: list of distractors at this moment we will set it to 3 distractors
+    """
+    sent_similarity_score = []
+    chunk_similarity_score = []
+    similar_chunks = []
+    similarity_sentence = []
+    for key, value in document.items():
+        # first we will look for similar sentences
+        for span_sentence in value:
+            score = key_sentence.similarity(span_sentence)
+            if score != 1 and score > 0.4:
+                sent_similarity_score.append(score)
+                similarity_sentence.append(span_sentence)
+    three_max_elem = np.array(sent_similarity_score).argsort()[-3:][::-1]  # returns indices of three max elements in  list
+
+    # Second step we will look for most similar noun chunks in those sentences
+    for sim_sent in similarity_sentence:
+        for noun_chunk in sim_sent.noun_chunks:
+            score = key_chunk.similarity(noun_chunk)
+            if 0.6 > score > 0.4:
+                chunk_similarity_score.append(score)
+                similar_chunks.append(noun_chunk)
+                # print(score, noun_chunk)
+    three_max_elem = np.array(chunk_similarity_score).argsort()[-3:][::-1]
+    return [similar_chunks[three_max_elem[0]], similar_chunks[three_max_elem[1]], similar_chunks[three_max_elem[2]]]
+
+
 def questions_formation(sentences, word_count, topic_words):
     # creating all possible key lists from selected sentences
     # need to identify noun_chunks
@@ -119,30 +153,40 @@ def questions_formation(sentences, word_count, topic_words):
     # f2 - contains in the title
     # f3 - height in syntactic tree
     weights = [1, 100, 1]
-    for span in sentences['information extraction']:
-        all_noun_chunks = []
-        print(span)
-        # Step 1: saving all noun chinks
-        for chunk in span.noun_chunks:
-            all_noun_chunks.append(chunk)
-        # Step 2: Selecting the most important noun chunk
-        for chunk in all_noun_chunks:
-            features = []
-            f1 = 0
-            f2 = 0
-            f3 = 0
-            for token in chunk:
-                if token.text.strip().lower() in word_count:
-                    f1 += word_count[token.text.strip().lower()]
-                if token.lemma_.strip().lower() in topic_words:
-                    f2 += 1
-                f3 += token_dep_height([token]) - 1
-            features.append(f1)
-            features.append(f2)
-            features.append(f3)
-            score = np.dot(weights, features)
-            print(score, chunk, features)
+    chunk_span_dict = {}
+    for topic in sentences:
+        for span in sentences[topic]: 
+            all_noun_chunks = []
+            # print(span)
+            # Step 1: saving all noun chinks
+            for chunk in span.noun_chunks:
+                all_noun_chunks.append(chunk)
+            # Step 2: Selecting the most important noun chunk
+            score = []
+            for chunk in all_noun_chunks:
+                features = []
+                f1 = 0
+                f2 = 0
+                f3 = 0
+                for token in chunk:
+                    if token.text.strip().lower() in word_count:
+                        f1 += word_count[token.text.strip().lower()]
+                    if token.lemma_.strip().lower() in topic_words:
+                        f2 += 1
+                    f3 += token_dep_height([token]) - 1
+                features.append(f1)
+                features.append(f2)
+                features.append(f3)
+                score.append(np.dot(weights, features))
 
+            # At this moment we choose max score chunk only, even though we can choose couple chunks with score > 100
+            gap_chunk_index = np.argmax(score)
+            if score:
+                if all_noun_chunks[gap_chunk_index] not in chunk_span_dict:
+                    chunk_span_dict[all_noun_chunks[gap_chunk_index]] = [span]
+                else:
+                    chunk_span_dict[all_noun_chunks[gap_chunk_index]].append(span)
+    return chunk_span_dict
 
 
 if __name__ == '__main__':
@@ -152,6 +196,20 @@ if __name__ == '__main__':
         book_text = f.read()
     data, word_dict = data_pre_processing(book_text)
     selected_sent, topic_words = sentence_selection(data)
-    questions_formation(selected_sent, word_dict, topic_words)
+    questions = questions_formation(selected_sent, word_dict, topic_words)
+    for key_chunk, value in questions.items():
+        for q in value:
+            distractor_list = distractor_selection(key_chunk, q, data, word_dict)
+            distractor_list.append(str(key_chunk))
+            shuffle(distractor_list)
+            # Printing question and multiple answers to this question:
+            gap_question = str(q).replace(str(key_chunk), '______________')
+            print('Question: ', gap_question)
+            print('a) ', distractor_list[0])
+            print('b) ', distractor_list[1])
+            print('c) ', distractor_list[2])
+            print('d) ', distractor_list[3])
+            print('Answer: ', key_chunk)
+            print('\n')
     # for i in range(0, len(data['named entity recognition'])):
     #     print(score['named entity recognition'][i], data['named entity recognition'][i])
