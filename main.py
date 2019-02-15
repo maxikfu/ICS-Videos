247f3d5db1605fc10f0a1be9baf6aa122bf367f9
megorov <megorov@central.uh.edu>
2019-02-14 19:40:27 -0600

    store sentences in list not dict. added comparison og NN, NNS, NNp, NNPS with video lecture. changed a little bit feature selection in senetnce selection

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ if __name__ == '__main__':
     #     for frame, region_ids in frames.items():
     #         for region_id, words in region_ids.items():
     #             pass
+    already_selected = set()
     for seg in dict_ocr:
         print(seg)
         segment = dict_ocr[seg]
@@ -22,8 +23,8 @@ if __name__ == '__main__':
         doc = nlp(' '.join(segment_text))
         video_words = set()
         for token in doc:
-            if not GFQG.is_stop(token.text) and not token.is_punct:
+            if not GFQG.is_stop(token.text) and not token.is_punct and token.tag_ in ['NN', 'NNS', 'NNP', 'NNPS']:
                 video_words.add(token.lemma_)
         # for slide, clusters in dict_ocr.items():
         #     list_of_words = list_of_words + [clusters[s][0][0] for s in clusters]
-        GFQG.rawtext2question('data/v4557/tt_anatomy_physiology.txt', video_words)
+        already_selected = GFQG.rawtext2question('data/v4557/tt_anatomy_physiology.txt', video_words, already_selected)
