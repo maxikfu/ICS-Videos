9b90170af42999d859638b50dd65b16af6b21e97
megorov <megorov@central.uh.edu>
2019-03-08 18:32:38 -0600

    saving to files as json to speed up process

--- NewApproach/ranking.py
+++ NewApproach/ranking.py
@@ -3,48 +3,44 @@ import pprint
 import json
 
 
+def text_ranking(book_segment):
+    """
+    :param book_segment: book segment in json format
+    :return: key sentences and key phrases
+    """
+    path_stage1 = "o1.json"
+    path_stage2 = "key_ph.json"
+    path_stage3 = "imp_sent.json"
 
-with open('tt_Earth_cleaned.json', 'r') as f:
-    stage0 = json.loads(f.readlines()[42])
+    """Perform statistical parsing/tagging on a document in JSON format"""
+    with open(path_stage1, 'w') as f:
+        for graf in pytextrank.parse_doc([book_segment]):
+            f.write("%s\n" % pytextrank.pretty_print(graf._asdict()))
 
-# pprint.pprint(stage0['text'])
-# exit()
+    graph, ranks = pytextrank.text_rank(path_stage1)
+    """Collect and normalize the key phrases from a parsed document"""
 
-path_stage1 = "o1.json"
-path_stage2 = "o2.json"
-path_stage3 = "o3.json"
+    key_phrases = list(pytextrank.normalize_key_phrases(path_stage1, ranks))
+    with open(path_stage2, 'w') as f:
+        for rl in key_phrases:
+            f.write("%s\n" % pytextrank.pretty_print(rl._asdict()))
 
-"""Perform statistical parsing/tagging on a document in JSON format"""
-with open(path_stage1, 'w') as f:
-    for graf in pytextrank.parse_doc([stage0]):
-        f.write("%s\n" % pytextrank.pretty_print(graf._asdict()))
-        # to view output in this notebook
-        # print(pytextrank.pretty_print(graf))
-graph, ranks = pytextrank.text_rank(path_stage1)
-"""Collect and normalize the key phrases from a parsed document"""
-with open(path_stage2, 'w') as f:
-    for rl in pytextrank.normalize_key_phrases(path_stage1, ranks):
-        f.write("%s\n" % pytextrank.pretty_print(rl._asdict()))
-        # to view output in this notebook
-        # print(pytextrank.pretty_print(rl))
-kernel = pytextrank.rank_kernel(path_stage2)
-"""Calculate a significance weight for each sentence, 
-using MinHash to approximate a Jaccard distance from key phrases determined by TextRank"""
-with open(path_stage3, 'w') as f:
-    for s in pytextrank.top_sentences(kernel, path_stage1):
-        f.write(pytextrank.pretty_print(s._asdict()))
-        f.write("\n")
-        # to view output in this notebook
+    kernel = pytextrank.rank_kernel(path_stage2)
+    """Calculate a significance weight for each sentence, 
+    using MinHash to approximate a Jaccard distance from key phrases determined by TextRank"""
+    key_sentences = list(pytextrank.top_sentences(kernel, path_stage1))
+    with open(path_stage3, 'w') as f:
+        for s in key_sentences:
+            f.write(pytextrank.pretty_print(s._asdict()))
+            f.write("\n")
+    return key_sentences, key_phrases
 
-"""Summarize a document based on most significant sentences and key phrases"""
-phrases = ", ".join(set([p for p in pytextrank.limit_keyphrases(path_stage2, phrase_limit=20)]))
-sent_iter = sorted(pytextrank.limit_sentences(path_stage3, word_limit=500), key=lambda x: x[1])
 
-s = []
+if __name__ == '__main__':
+    with open('tt_Earth_cleaned.json', 'r') as f:
+        stage0 = json.loads(f.readlines()[42])
+    key_sent, key_phr = text_ranking(stage0)
+    for s in key_sent:
+        print(s)
 
-for sent_text, idx in sent_iter:
-    s.append(pytextrank.make_sentence(sent_text))
-
-graf_text = " ".join(s)
-pprint.pprint("**excerpts:** %s\n\n**keywords:** %s" % (graf_text, phrases,))
 
