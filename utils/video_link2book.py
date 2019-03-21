c8e144628550d70e319261914ee2f0c311779f78
megorov <megorov@central.uh.edu>
2019-03-21 17:30:19 -0500

    in each video segment folder copy frame images. Easy to rate relevance that way

--- utils/video_link2book.py
+++ utils/video_link2book.py
@@ -61,7 +61,7 @@ def video_2book(video_id, path_to_book):
 
 if __name__ == '__main__':
     #videos_id = [4853, 4887, 4916, 4954, 4984, 4998, 5019, 5030, 5039, 5056, 5063, 5072, 5088]
-    videos_id = [4608]
+    videos_id = [4588]
     for v in videos_id:
         print(v)
         video_2book(v, '../data/GEOL1330Fall18_Jinny/tt_Earth_cleaned.json')
